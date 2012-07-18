c1369529302e0491d88681a8ae4c6ae17182c333
Jason Parker <kornyone@gmail.com>
2012-07-17 23:49:32 -0500

    Use tvdpi

--- device.mk
+++ device.mk
@@ -25,8 +25,8 @@ DEVICE_PACKAGE_OVERLAYS := device/samsung/tuna/overlay
 # currently contain all of the bitmaps at xhdpi density so
 # we do this little trick to fall back to the hdpi version
 # if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
-PRODUCT_AAPT_PREF_CONFIG := xhdpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi tvdpi
+PRODUCT_AAPT_PREF_CONFIG := tvdpi
 
 PRODUCT_PACKAGES := \
 	lights.tuna
